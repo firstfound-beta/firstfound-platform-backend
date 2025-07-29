a24ea6a1480310aa80093e7f9ac768c58b418424
bhavanishanker <bhavanishankerpichuka@gmail.com>
2025-07-29 19:11:47 +0530

    feat: add Swagger integration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger configuration
+  const config = new DocumentBuilder()
+    .setTitle('Your API Title')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addBearerAuth() // If you're using JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document); // Swagger at /api
+
+  await app.listen(3000);
 }
 bootstrap();

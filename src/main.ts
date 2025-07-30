50bd105d2e8dac2e99bcc5ef0c4bdbfb2acc2cd6
Priyanshu Kumar <136223448+PriyanshuKumar9798@users.noreply.github.com>
2025-07-31 00:41:49 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://firstfound-platform-frontend.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  });
   // Swagger configuration
   const config = new DocumentBuilder()
     .setTitle('Your API Title')
